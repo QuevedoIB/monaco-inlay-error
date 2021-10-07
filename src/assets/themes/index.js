7ffb9f8c9789c38951c9927d142657bdedb40f17
Ivan <4esoquevedoi@gmail.com>
2021-10-07 10:29:59 +0200

    initial config

---
+++ src/assets/themes/index.js
@@ -0,0 +1,1 @@
+export * as solarized from "./solarized.json"
---
+++ src/main.js
@@ -0,0 +1,91 @@
+import { monaco } from "./monaco/index";
+import { emmetHTML } from "emmet-monaco-es";
+import { loadWASM } from "onigasm";
+import { Registry } from "monaco-textmate";
+import { wireTmGrammars } from "monaco-editor-textmate";
+import * as syntaxes from "../src/assets/syntaxes";
+
+import "../style.css";
+
+monaco.languages.register({ id: "javascript" });
+monaco.languages.register({ id: "typescript" });
+monaco.languages.register({ id: "css" });
+monaco.languages.register({ id: "html" });
+
+emmetHTML(monaco);
+
+export async function createEditors(configs) {
+  try {
+    const response = await window.fetch(
+      "https://cdn.jsdelivr.net/npm/onigasm@2.2.5/lib/onigasm.wasm"
+    );
+    const buffer = await response.arrayBuffer();
+    await loadWASM(buffer);
+
+    const registry = new Registry({
+      getGrammarDefinition: async (scopeName) => {
+        try {
+          const extension = scopeName.split(".")[1];
+          const data = {
+            format: "json",
+            content: JSON.stringify(syntaxes[extension]),
+          };
+          return data;
+        } catch (error) {
+          console.log("Failed to load tmLanguages files", error);
+        }
+      },
+    });
+
+    const grammars = new Map();
+    grammars.set("css", "source.css");
+    grammars.set("html", "text.html.basic");
+    grammars.set("typescript", "source.ts");
+    grammars.set("javascript", "source.js");
+
+    const configureTheme = async () => {
+      await import(
+        "monaco-editor/esm/vs/language/typescript/monaco.contribution"
+      );
+      await import("monaco-editor/esm/vs/language/css/monaco.contribution");
+      await import("monaco-editor/esm/vs/language/html/monaco.contribution");
+
+      monaco.editor.defineTheme("custom-theme", { base: "vs" });
+    };
+
+    await configureTheme();
+
+    const editors = {};
+
+    await Promise.all(
+      configs.map(async ({ language, value, domElement }) => {
+        const editor = monaco.editor.create(domElement, {
+          value,
+          language,
+        });
+        editors[language] = editor;
+        return await wireTmGrammars(monaco, registry, grammars, editor);
+      })
+    );
+
+    return editors;
+  } catch (error) {
+    console.log("Error while setting up editors", error);
+  }
+}
+
+(async () => {
+  await createEditors([
+    {
+      domElement: document.querySelector(".html"),
+      language: "html",
+      value: ``,
+    },
+    { domElement: document.querySelector(".css"), language: "css", value: `` },
+    {
+      domElement: document.querySelector(".js"),
+      language: "javascript",
+      value: ``,
+    },
+  ]);
+})();
